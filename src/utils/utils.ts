76cb41fc5a30525ab76762050d5dc6a4bd35576e
Matthew <matthew@edge.app>
2021-07-27 14:10:18 -0700

    Add fiat utility functions

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,6 @@
 import { validate } from 'jsonschema'
 
-import { NewRates, ReturnRate } from '../rates'
+import { NewRates, RateMap, ReturnRate } from '../rates'
 import { config } from './../config'
 import { constantCurrencyCodes } from './currencyCodeMaps.json'
 
@@ -46,12 +46,6 @@ export const haveEveryRate = (rates: ReturnRate[]): boolean => {
   return rates.every(rate => rate.exchangeRate !== null)
 }
 
-export const invertPair = (pair: string): string => {
-  const fromCurrency = pair.split('_')[0]
-  const toCurrency = pair.split('_')[1]
-  return `${toCurrency}_${fromCurrency}`
-}
-
 export const checkConstantCode = (code: string): string =>
   constantCurrencyCodes[code] ?? code
 
@@ -91,11 +85,59 @@ export const logger = (...args): void => {
   console.log(result)
 }
 
-export const defaultFiatPair = (code: string): string =>
-  `${code}_${DEFAULT_FIAT}`
+type IsoOp = (code: string) => string
+type IsoOpObject = (code: any) => string
 
 export const isFiatCode = (code: string): boolean => code.indexOf('iso:') === 0
 
-export const subIso = (code: string): string => code.split(':')[1]
+export const subIso: IsoOp = code =>
+  isFiatCode(code) ? code.split(':')[1] : code
 
-export const addIso = (code: string): string => `iso:${code}`
+export const addIso: IsoOp = code => (!isFiatCode(code) ? `iso:${code}` : code)
+
+export const toCurrencyPair = (
+  codeA: string,
+  codeB: string = DEFAULT_FIAT
+): string => `${codeA}_${codeB}`
+
+export const toIsoPair = (opA: IsoOp, opB: IsoOp = opA) => (
+  codeA: string,
+  codeB: string = DEFAULT_FIAT
+) => toCurrencyPair(opA(codeA), opB(codeB))
+
+export const fromFiatToFiat = toIsoPair(addIso, addIso)
+
+export const fromCryptoToFiatCurrencyPair = toIsoPair(subIso, addIso)
+
+export const currencyCodeArray = (pair: string): string[] => pair.split('_')
+
+export const fromCode = (pair: string): string => currencyCodeArray(pair)[0]
+
+export const toCode = (pair: string): string => currencyCodeArray(pair)[1]
+
+export const invertPair = (pair: string): string =>
+  `${toCode(pair)}_${fromCode(pair)}`
+
+export const createReducedRateMapArray = <T>(
+  createCurrencyPair: IsoOpObject,
+  createCurrencyQuote: (code: T) => string
+) => (data: T[]): RateMap =>
+  data.reduce((out, code) => {
+    return {
+      ...out,
+      [createCurrencyPair(code)]: createCurrencyQuote(code)
+    }
+  }, {})
+
+export const createReducedRateMap = <T>(
+  createCurrencyPair: IsoOp,
+  createCurrencyQuote: (rates: T, code: string) => string
+) => (data: T): RateMap =>
+  Object.keys(data).reduce((out, code) => {
+    return {
+      ...out,
+      [createCurrencyPair(code)]: createCurrencyQuote(data, code)
+    }
+  }, {})
+
+export const dateOnly = (date: string): string => date.split('T')[0]
