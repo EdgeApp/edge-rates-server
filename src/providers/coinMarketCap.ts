f97251b633976b76a337fdcf4d451ff9e72b5f1b
Matthew <matthew@edge.app>
2021-07-27 23:19:58 -0700

    Consolidate coinmarketcap queries into single file

--- src/providers/coinMarketCap.ts
+++ src/providers/coinMarketCap.ts
@@ -23,19 +23,84 @@ object if only one currency is requested. They ignore unrecognized codes so
 this ensures the response will have at least two accepted currency codes.
 */
 
+const DEFAULT_CODES = ['BTC', 'ETH']
+
 const {
   providers: {
-    coinMarketCapHistorical: { uri, apiKey }
+    coinMarketCapHistorical: { uri: historicalUri, apiKey: historicalApiKey },
+    coinMarketCapCurrent: { uri: currentUri, apiKey: currentapiKey }
   },
   defaultFiatCode: DEFAULT_FIAT
 } = config
 
-const DEFAULT_CODES = ['BTC', 'ETH']
-
-const OPTIONS = {
+const CURRENT_OPTIONS = {
   method: 'GET',
   headers: {
-    'X-CMC_PRO_API_KEY': apiKey
+    'X-CMC_PRO_API_KEY': currentapiKey
+  },
+  json: true
+}
+
+const asCoinMarketCapCurrentQuotes = asMap(
+  asObject({
+    quote: asMap(asObject({ price: asNumber }))
+  })
+)
+
+const asCoinMarketCapCurrentResponse = asObject({
+  data: asCoinMarketCapCurrentQuotes
+})
+
+const coinMarketCapCurrentQuote = (
+  data: ReturnType<typeof asCoinMarketCapCurrentQuotes>,
+  code: string
+): string => data[code].quote[subIso(DEFAULT_FIAT)].price.toString()
+
+const coinMarketCapCurrentRateMap = createReducedRateMap(
+  fromCryptoToFiatCurrencyPair,
+  coinMarketCapCurrentQuote
+)
+
+export const coinMarketCapCurrent = async (
+  date: string,
+  codes: string[]
+): Promise<NewRates> => {
+  const rates = { [date]: {} }
+
+  if (currentapiKey == null) {
+    logger('No coinMarketCapCurrent API key')
+    return rates
+  }
+
+  // Query
+  if (codes.length > 0)
+    try {
+      const response = await fetch(
+        `${currentUri}/v1/cryptocurrency/quotes/latest?symbol=${codes.join(
+          ','
+        )}&skip_invalid=true&convert=${subIso(DEFAULT_FIAT)}`,
+        CURRENT_OPTIONS
+      )
+      if (response.status !== 200) {
+        logger(
+          `coinMarketCapCurrent returned code ${response.status} for ${codes} at ${date}`
+        )
+        throw new Error(response.statusText)
+      }
+      const json = asCoinMarketCapCurrentResponse(await response.json())
+
+      // Create return object
+      rates[date] = coinMarketCapCurrentRateMap(json.data)
+    } catch (e) {
+      logger(`No coinMarketCapCurrent quote: ${JSON.stringify(e)}`)
+    }
+  return rates
+}
+
+const HISTORICAL_OPTIONS = {
+  method: 'GET',
+  headers: {
+    'X-CMC_PRO_API_KEY': historicalApiKey
   },
   json: true
 }
@@ -70,15 +135,20 @@ const coinMarketCapHistoricalRateMap = createReducedRateMap(
   coinMarketCapHistoricalQuote
 )
 
-const query = async (date: string, codes: string[]): Promise<NewRates> => {
+const coinMarketCapHistorical = async (
+  date: string,
+  codes: string[]
+): Promise<NewRates> => {
   const rates = {}
   if (codes.length === 0) return rates
   try {
     const response = await fetch(
-      `${uri}/v1/cryptocurrency/quotes/historical?symbol=${codes}&time_end=${date}&count=1&skip_invalid=true&convert=${subIso(
+      `${historicalUri}/v1/cryptocurrency/quotes/historical?symbol=${codes.join(
+        ','
+      )}&time_end=${date}&count=1&skip_invalid=true&convert=${subIso(
         DEFAULT_FIAT
       )}`,
-      OPTIONS
+      HISTORICAL_OPTIONS
     )
     if (response.status !== 200 || response.ok === false) {
       logger(
@@ -96,12 +166,13 @@ const query = async (date: string, codes: string[]): Promise<NewRates> => {
   return rates
 }
 
-export const coinMarketCapHistorical = async (
-  rateObj: ReturnRate[]
+export const coinMarketCap = async (
+  rateObj: ReturnRate[],
+  currentTime: string
 ): Promise<NewRates> => {
   const rates = {}
 
-  if (apiKey == null) {
+  if (historicalApiKey == null) {
     logger('No coinMarketCapHistorical API key')
     return rates
   }
@@ -119,9 +190,11 @@ export const coinMarketCapHistorical = async (
   }
 
   // Query
-  const providers = Object.keys(datesAndCodesWanted).map(async date =>
-    query(date, datesAndCodesWanted[date])
-  )
+  const providers = Object.keys(datesAndCodesWanted).map(async date => {
+    if (date === currentTime)
+      return coinMarketCapCurrent(date, datesAndCodesWanted[date])
+    return coinMarketCapHistorical(date, datesAndCodesWanted[date])
+  })
   try {
     const response = await Promise.all(providers)
     combineRates(rates, response)
@@ -138,8 +211,8 @@ const asCoinMarketCapAssetResponse = asObject({
 
 export const coinMarketCapAssets = async (): Promise<AssetMap> => {
   const response = await fetch(
-    `${uri}/v1/cryptocurrency/map?limit=5000`,
-    OPTIONS
+    `${historicalUri}/v1/cryptocurrency/map?limit=5000`,
+    HISTORICAL_OPTIONS
   )
   if (response.ok === false) {
     throw new Error(response.status)
