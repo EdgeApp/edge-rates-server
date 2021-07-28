92a4037eb751226ebd9873fddc580cf8d7d410b9
Matthew <matthew@edge.app>
2021-07-28 01:05:59 -0700

    Add unique ID support to createReducedRateMap util

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,6 @@
 import { validate } from 'jsonschema'
 
-import { NewRates, RateMap, ReturnRate } from '../rates'
+import { AssetMap, NewRates, RateMap, ReturnRate } from '../rates'
 import { config } from './../config'
 import { constantCurrencyCodes } from './currencyCodeMaps.json'
 
@@ -129,14 +129,20 @@ export const createReducedRateMapArray = <T>(
     }
   }, {})
 
+const useCurrencyCodeAsIs = (code: string): string => code
+
 export const createReducedRateMap = <T>(
   createCurrencyPair: IsoOp,
-  createCurrencyQuote: (rates: T, code: string) => string
-) => (data: T): RateMap =>
+  createCurrencyQuote: (rates: T, code: string) => string,
+  uniqueId: (id: string, assetMap: AssetMap) => string = useCurrencyCodeAsIs
+) => (data, assetMap = {}): RateMap =>
   Object.keys(data).reduce((out, code) => {
     return {
       ...out,
-      [createCurrencyPair(code)]: createCurrencyQuote(data, code)
+      [createCurrencyPair(uniqueId(code, assetMap))]: createCurrencyQuote(
+        data,
+        code
+      )
     }
   }, {})
 
