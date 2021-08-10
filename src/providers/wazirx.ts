46932f07d39ca5ff95f10637a8be4bcdfd6aaae0
Matthew <matthew@edge.app>
2021-08-09 17:00:49 -0700

    Add preferred providers for INR and ARS fiat currencies
    Also moved currencyConverter near the end of the list. Looking up fiat/fiat exchange rates last ensures that the currency bridge doesn't use these new one-off btc/fiat rates to fill in the rest of the requested rates.

---
+++ src/providers/wazirx.ts
@@ -0,0 +1,36 @@
+import { asObject, asString } from 'cleaners'
+import fetch from 'node-fetch'
+
+import { config } from '../config'
+import { NewRates, RateMap, ReturnRate } from './../rates'
+import { logger } from './../utils/utils'
+
+const { uri } = config.providers.wazirx
+
+const asWazirxResponse = asObject({ btcinr: asObject({ last: asString }) })
+
+const wazirxRateMap = (
+  results: ReturnType<typeof asWazirxResponse>
+): RateMap => ({ 'BTC_iso:INR': results.btcinr.last })
+
+const wazirx = async (
+  requestedRates: ReturnRate[],
+  currentTime: string
+): Promise<NewRates> => {
+  const rates = { [currentTime]: {} }
+
+  // Query
+  try {
+    const response = await fetch(`${uri}/api/v2/tickers`)
+    if (response.status !== 200) {
+      throw new Error(response.statusText)
+    }
+    const json = asWazirxResponse(await response.json())
+    rates[currentTime] = wazirxRateMap(json)
+  } catch (e) {
+    logger(`No wazirx quote: ${e.message}`)
+  }
+  return rates
+}
+
+export { wazirx }
