4a2c7921acd7fff73d09e33d48d3664148804e76
Matthew <matthew@edge.app>
2021-08-11 15:29:09 -0700

    Remove await-lock

--- src/utils/dbUtils.ts
+++ src/utils/dbUtils.ts
@@ -1,45 +1,34 @@
-import AwaitLock from 'await-lock'
-
 import { DbDoc } from '../rates'
 import { config } from './../config'
 import currencyCodeMaps from './currencyCodeMaps.json'
 import { slackPoster } from './postToSlack'
 import { logger } from './utils'
 
-let LOCK_ID = 0
-
-export const saveToDb = (localDB: any, docs: DbDoc[], locks = {}): void => {
+export const saveToDb = (localDB: any, docs: DbDoc[]): void => {
   const db: string = localDB?.config?.db ?? ''
   if (docs.length === 0) return
-  locks[++LOCK_ID] = new AwaitLock()
-  locks[LOCK_ID].acquireAsync().then(() =>
-    localDB
-      .bulk({ docs })
-      .then(response => {
-        const successArray = response
-          .filter(doc => doc.error == null)
-          .map(doc => doc.id)
-        if (successArray.length > 0)
-          logger(`Saved document IDs: ${successArray.join(', ')} to db: ${db}`)
+  localDB
+    .bulk({ docs })
+    .then(response => {
+      const successArray = response
+        .filter(doc => doc.error == null)
+        .map(doc => doc.id)
+      if (successArray.length > 0)
+        logger(`Saved document IDs: ${successArray.join(', ')} to db: ${db}`)
 
-        const failureArray = response
-          .filter(doc => doc.error != null)
-          .map(doc => `${doc.id}: ${doc.error}`)
-        if (failureArray.length > 0)
-          logger(
-            `Error saving document IDs: ${failureArray.join(', ')} to db: ${db}`
-          )
-      })
-      .catch(e => {
-        slackPoster(config.slackWebhookUrl, e).catch(e)
-      })
-      .finally(() => {
-        locks[LOCK_ID].release()
-        if (locks[LOCK_ID]._waitingResolvers.length === 0) {
-          delete locks[LOCK_ID]
-        }
-      })
-  )
+      const failureArray = response
+        .filter(doc => doc.error != null)
+        .map(doc => `${doc.id}: ${doc.error}`)
+      if (failureArray.length > 0)
+        logger(
+          `Error saving document IDs: ${failureArray.join(', ')} to db: ${db}`
+        )
+    })
+    .catch(e => {
+      console.log(e.message)
+      logger(e.message)
+      slackPoster(config.slackWebhookUrl, e).catch(e)
+    })
 }
 
 export const getFromDb = async (
