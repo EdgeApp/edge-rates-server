d1e317854e0071f07edebe4a5edf9a16e9b80115
Matthew <matthew@edge.app>
2021-08-12 22:43:23 -0700

    Move db initialization into indexEngines

--- src/indexEngines.ts
+++ src/indexEngines.ts
@@ -1,3 +1,13 @@
-import { ratesEngine } from './ratesEngine'
+import { setupDatabase } from 'edge-server-tools'
 
-ratesEngine().catch(e => console.log(e))
+import { config } from './config'
+import { ratesEngine } from './ratesEngine'
+import { ratesDbSetup } from './utils/dbUtils'
+
+// Initialize DB
+async function initDb(): Promise<void> {
+  await setupDatabase(config.couchUri, ratesDbSetup)
+  ratesEngine().catch(e => console.log(e))
+}
+
+initDb().catch(e => console.log(e))
