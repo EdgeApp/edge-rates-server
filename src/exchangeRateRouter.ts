e3369e577728a16ebad01a9980ce11952e83b547
Matthew <matthew@edge.app>
2021-08-20 12:59:43 -0700

    Break apart routes into composable functions

--- src/exchangeRateRouter.ts
+++ src/exchangeRateRouter.ts
@@ -7,7 +7,7 @@ import express from 'express'
 import nano from 'nano'
 import promisify from 'promisify-node'
 
-import { asConfig } from './config'
+import { config } from './config'
 import { getExchangeRates } from './rates'
 import { DbDoc } from './utils/dbUtils'
 
@@ -24,56 +24,81 @@ export type ExchangeRateReq = ReturnType<typeof asExchangeRateReq>
 
 const EXCHANGE_RATES_BATCH_LIMIT = 100
 
-export const exchangeRateRouter = (
-  config: ReturnType<typeof asConfig>
-): express.Router => {
+// Nano for CouchDB
+// =============================================================================
+const nanoDb = nano(config.couchUri)
+const dbRates: nano.DocumentScope<DbDoc> = nanoDb.db.use('db_rates')
+promisify(dbRates)
+
+// Query params:
+//  currency_pair: String with the two currencies separated by an underscore. Ex: "ETH_iso:USD"
+//  date: String (optional) ex. "2019-11-21T15:28:21.123Z"
+
+const exchangeRateCleaner = (req: any, res: any, next: Function): void => {
+  const { currencyPair, date } = req.query
+  try {
+    req.requestedRates = asExchangeRatesReq({
+      data: [{ currency_pair: currencyPair, date }]
+    })
+  } catch (e) {
+    return res.status(400).send(`Missing Request fields.`)
+  }
+  next()
+}
+
+//  Query body:
+//  { data: [{ currency_pair: string, date? string) }] }
+
+const exchangeRatesCleaner = (req: any, res: any, next: Function): void => {
+  try {
+    req.requestedRates = asExchangeRatesReq(req.body)
+  } catch (e) {
+    return res.status(400).send(`Missing Request fields.`)
+  }
+  if (req.requestedRates.data.length > EXCHANGE_RATES_BATCH_LIMIT) {
+    return res
+      .status(400)
+      .send(`Exceeded Limit of ${EXCHANGE_RATES_BATCH_LIMIT}`)
+  }
+  next()
+}
+
+const queryExchangeRates = async (
+  req: any,
+  res: any,
+  next: Function
+): Promise<void> => {
+  req.requestedRatesResult = await getExchangeRates(
+    req.requestedRates.data,
+    dbRates
+  )
+  next()
+}
+
+const sendExchangeRate = (req: any, res: any): void => {
+  res.json(req.requestedRatesResult.data[0])
+}
+
+const sendExchangeRates = (req: any, res: any): void => {
+  res.json({ data: req.requestedRatesResult.data })
+}
+
+export const exchangeRateRouter = (): express.Router => {
   // ROUTES FOR OUR API
   // =============================================================================
   const router = express.Router()
 
-  // Nano for CouchDB
-  // =============================================================================
-  const nanoDb = nano(config.couchUri)
-  const dbRates: nano.DocumentScope<DbDoc> = nanoDb.db.use('db_rates')
-  promisify(dbRates)
+  router.get('/exchangeRate', [
+    exchangeRateCleaner,
+    queryExchangeRates,
+    sendExchangeRate
+  ])
 
-  /*
-   * Query params:
-   * currency_pair: String with the two currencies separated by an underscore. Ex: "ETH_iso:USD"
-   */
-  router.get('/exchangeRate', async function(req, res) {
-    const currencyPair = req.query.currency_pair
-    const date = req.query.date
-    let query
-    try {
-      query = asExchangeRatesReq({
-        data: [{ currency_pair: currencyPair, date }]
-      })
-    } catch (e) {
-      return res.status(400).send(`Missing Request fields.`)
-    }
-
-    const result = await getExchangeRates(query.data, dbRates)
-    res.json(result.data[0])
-  })
-
-  router.post('/exchangeRates', async function(req, res) {
-    let query
-    try {
-      query = asExchangeRatesReq(req.body)
-    } catch (e) {
-      return res.status(400).send(`Missing Request fields.`)
-    }
-    if (query.data.length > EXCHANGE_RATES_BATCH_LIMIT) {
-      return res
-        .status(400)
-        .send(`Exceeded Limit of ${EXCHANGE_RATES_BATCH_LIMIT}`)
-    }
-
-    const requestedRates: ExchangeRateReq[] = query.data
-    const data = await getExchangeRates(requestedRates, dbRates)
-    res.json({ data: data.data })
-  })
+  router.post('/exchangeRates', [
+    exchangeRatesCleaner,
+    queryExchangeRates,
+    sendExchangeRates
+  ])
 
   return router
 }
