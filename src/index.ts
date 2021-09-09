5d0e11bc8fbee253517e0ad7fa86d4666e6d31f8
Matthew <matthew@edge.app>
2021-09-08 17:47:22 -0700

    Add additional comments clarifying difference between v1 and v2

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,9 @@ import { logger } from './utils/utils'
 
 // Create Router
 const router = createRouter({
-  // Create Exchange Rate Router
+  // v1 API consumer sends currency codes without any prefixes and the server makes a best guess at the type using a local list of fiat codes before passing to data providers
   '/v1': exchangeRateRouterV1(),
+  // v2 API consumer indicates currency code type in the request by prefixing fiat codes with "iso:"
   '/v2': exchangeRateRouterV2()
 })
 // Create Server
