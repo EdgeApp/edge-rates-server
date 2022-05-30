fc77946c0683c1482fcabe3441721538b63bfbc9
Matthew <matthew@edge.app>
2022-05-30 15:31:55 -0700

    Delay engine start by a second
    The rates engine started it's first query before redis was populated

--- src/indexEngines.ts
+++ src/indexEngines.ts
@@ -4,10 +4,12 @@ import { config } from './config'
 import { ratesEngine } from './ratesEngine'
 import { uidEngine } from './uidEngine'
 import { ratesDbSetup } from './utils/dbUtils'
+import { snooze } from './utils/utils'
 
 // Initialize DB
 async function initDb(): Promise<void> {
   await setupDatabase(config.couchUri, ratesDbSetup)
+  await snooze(1000) // give redis cache a moment to get updated from couch
   ratesEngine().catch(e => console.log(e))
   uidEngine().catch(e => console.log(e))
 }
