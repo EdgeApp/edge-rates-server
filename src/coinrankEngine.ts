b9a37eb3f438d3e49a3ce9c22af1549c2c215bab
Paul V Puey <paul@edge.app>
2023-01-04 22:03:31 -0800

    fixup! Add coinrank engine to query coingecko every 45s

--- src/coinrankEngine.ts
+++ src/coinrankEngine.ts
@@ -1,26 +1,21 @@
 import fetch from 'node-fetch'
-import { createClient } from 'redis'
 
 import { config } from './config'
 import { REDIS_COINRANK_KEY_PREFIX } from './constants'
-import { asCoingeckoMarkets, CoinrankRedis } from './types'
+import { asCoingeckoMarkets, CoinrankMarkets, CoinrankRedis } from './types'
 import { setAsync, slackMessage } from './utils/dbUtils'
 import { logger, snooze } from './utils/utils'
 
-const client = createClient()
-client.connect().catch(e => logger('redis connect error: ', e))
-
 const PAGE_SIZE = 250
 const LOOP_DELAY = 45000
+const { defaultFiatCode } = config
 
 export const coinrankEngine = async (): Promise<void> => {
-  await console.log('hello')
-
   logger('Updating Coinrank Cache')
   try {
     const lastUpdate = new Date().toISOString()
     const { uri } = config.providers.coingecko
-    let markets: any = []
+    let markets: CoinrankMarkets = []
     for (let page = 1; page <= 8; page++) {
       const url = `${uri}/api/v3/coins/markets?vs_currency=USD&page=${page}&per_page=${PAGE_SIZE}&price_change_percentage=1h,24h,7d,14d,30d,1y`
       const response = await fetch(url)
@@ -29,15 +24,14 @@ export const coinrankEngine = async (): Promise<void> => {
         throw new Error(text)
       }
       const reply = await response.json()
-      try {
-        const marketsPage = asCoingeckoMarkets(reply)
-        markets = [...markets, ...marketsPage]
-      } catch (e) {
-        console.log('ugh')
-      }
+      const marketsPage = asCoingeckoMarkets(reply)
+      markets = [...markets, ...marketsPage]
     }
     const data: CoinrankRedis = { lastUpdate, markets }
-    await setAsync(`${REDIS_COINRANK_KEY_PREFIX}_USD`, JSON.stringify(data))
+    await setAsync(
+      `${REDIS_COINRANK_KEY_PREFIX}_${defaultFiatCode}`,
+      JSON.stringify(data)
+    )
   } catch (e) {
     const err: any = e // Weird TS issue causing :any to get removed from above line
     const message = `coinrankEngine failure: ${err.message}`
