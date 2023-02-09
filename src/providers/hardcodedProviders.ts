da90e9a473b2878d91de7fafc9e8a7bd7d25e8f5
Matthew <matthew@edge.app>
2023-02-09 15:19:13 -0800

    Use case-appropriate values for assetMap and DB saved values
    This resolves issues with mixed-case currency codes like is used for XRP tokens. The assetMap should use the actual case-sensitive string like a client would request. The value that is added to the rates object, which is saved to the DB, needs to be all caps (except the fiat portion).  Unfortunately, since each provider handles currency code casing uniquely it kind of has to be handled here and not in a more general location which is why this is kind of ugly.

--- src/providers/hardcodedProviders.ts
+++ src/providers/hardcodedProviders.ts
@@ -1,6 +1,6 @@
 import { div } from 'biggystring'
 
-import { currencyCodeArray, invertPair } from '../utils/utils'
+import { currencyCodeArray, fromCode, toCode } from '../utils/utils'
 import { AssetMap, NewRates, PRECISION, ReturnRate } from './../rates'
 
 export const zeroRates = (
@@ -35,13 +35,23 @@ export const fallbackConstantRates = (
       rates[pair.date] = {}
     }
 
-    if (assetMap[pair.currency_pair] != null) {
-      rates[pair.date][pair.currency_pair] = assetMap[pair.currency_pair]
+    const requestedCurrencyPair = pair.currency_pair // exists in assetMap the same way a consumer would request it "USD.st_iso:USD"
+    const dbSafeCurrencyPair = `${fromCode(pair.currency_pair)}_${toCode(
+      pair.currency_pair
+    )}` // The format we would store in the database "USD.ST_iso:USD"
+
+    if (assetMap[requestedCurrencyPair] != null) {
+      rates[pair.date][dbSafeCurrencyPair] = assetMap[requestedCurrencyPair]
       continue
     }
-    if (assetMap[invertPair(pair.currency_pair)] != null) {
-      const invertedRate = assetMap[invertPair(pair.currency_pair)]
-      rates[pair.date][pair.currency_pair] =
+
+    const inversePair = pair.currency_pair
+      .split('_')
+      .reverse()
+      .join('_')
+    if (assetMap[inversePair] != null) {
+      const invertedRate = assetMap[inversePair]
+      rates[pair.date][dbSafeCurrencyPair] =
         invertedRate !== '0' ? div('1', invertedRate, PRECISION) : '0'
     }
   }
