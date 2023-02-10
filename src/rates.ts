84e8cf006c2169ec6169c4d31f8cadbd108ee6a6
Matthew <matthew@edge.app>
2023-02-10 11:57:32 -0800

    Add Map() reminder

--- src/rates.ts
+++ src/rates.ts
@@ -84,6 +84,7 @@ export interface NewRates {
   [date: string]: RateMap
 }
 
+// TODO: Future architecture changes should convert data types like these to Map()
 export interface AssetMap {
   [currencyCode: string]: string
 }
