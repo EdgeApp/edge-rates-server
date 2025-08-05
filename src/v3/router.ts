32516801615a18d68f4d15489bffe7aea6a43d4c
Matthew <matthew@edge.app>
2025-08-04 17:04:12 -0700

    Add date to incoming requests

--- src/v3/router.ts
+++ src/v3/router.ts
@@ -2,13 +2,49 @@ import { HttpResponse } from 'serverlet'
 import { ExpressRequest } from 'serverlet/express'
 
 import { getRates } from './getRates'
-import { asGetRatesParams } from './types'
+import {
+  asIncomingGetRatesParams,
+  GetRatesParams,
+  IncomingGetRatesParams
+} from './types'
+
+const ONE_MINUTE = 60 * 1000
+
+const fixIncomingGetRatesParams = (
+  rawParams: IncomingGetRatesParams
+): GetRatesParams => {
+  const rightNow = Date.now()
+  const normalizeTime = Math.floor(rightNow / ONE_MINUTE) * ONE_MINUTE
+  const normalizedIsoDate = new Date(normalizeTime)
+
+  const params = asIncomingGetRatesParams(rawParams)
+
+  params.crypto.forEach(crypto => {
+    // Sanity check that the tokenId doesn't include _
+    if (
+      typeof crypto.asset.tokenId === 'string' &&
+      crypto.asset.tokenId.includes('_')
+    ) {
+      throw new Error('tokenId cannot include _')
+    }
+    if (crypto.isoDate == null) {
+      crypto.isoDate = normalizedIsoDate
+    }
+  })
+  params.fiat.forEach(fiat => {
+    if (fiat.isoDate == null) {
+      fiat.isoDate = normalizedIsoDate
+    }
+  })
+
+  return params as GetRatesParams
+}
 
 export const ratesV3 = async (
   request: ExpressRequest
 ): Promise<HttpResponse> => {
   try {
-    const params = asGetRatesParams(request.req.body)
+    const params = fixIncomingGetRatesParams(request.req.body)
     const result = await getRates(params)
     return {
       headers: { 'content-type': 'application/json' },
