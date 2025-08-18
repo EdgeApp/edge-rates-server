0995addc387ce67926b0c86dc46535d56a1fd9f1
peachbits <matthew@edge.app>
2025-08-18 15:52:56 -0700

    fixup! Add crypto asset leaderboard

--- src/v3/constants.ts
+++ src/v3/constants.ts
@@ -3,5 +3,5 @@ export const FIVE_MINUTES = 5 * ONE_MINUTE
 export const ONE_HOUR = 60 * ONE_MINUTE
 export const TWENTY_FOUR_HOURS = 24 * ONE_HOUR
 
-export const LEADERBOARD_KEY = 'edgerates:top100'
+export const LEADERBOARD_KEY = 'edgerates:topAssets'
 export const TOKEN_TYPES_KEY = 'tokenTypes'
