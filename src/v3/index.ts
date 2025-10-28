13333e74db7d3207a2417cff5808697c9517c578
Paul Puey <paul@edge.app>
2025-10-27 17:35:36 -0700

    Allow cors

--- src/v3/index.ts
+++ src/v3/index.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import http from 'http'
-import { pickMethod, pickPath } from 'serverlet'
+import { pickMethod, pickPath, withCors } from 'serverlet'
 import { makeExpressRoute } from 'serverlet/express'
 
 import { config } from '../config'
@@ -18,15 +18,17 @@ async function main(): Promise<void> {
 }
 
 function server(): void {
-  const server = pickPath({
-    '/': pickMethod({ GET: heartbeatV3 }),
-    '/v2/exchangeRate': pickMethod({ GET: rateV2 }),
-    '/v2/exchangeRates': pickMethod({ POST: ratesV2 }),
-    '/v2/coinrank': pickMethod({ GET: sendCoinranksV2 }),
-    '/v2/coinrankAsset/([^/]+)': pickMethod({ GET: sendCoinrankAssetV2 }),
-    '/v2/coinrankList': pickMethod({ GET: sendCoinrankListV2 }),
-    '/v3/rates': pickMethod({ POST: ratesV3 })
-  })
+  const server = withCors(
+    pickPath({
+      '/': pickMethod({ GET: heartbeatV3 }),
+      '/v2/exchangeRate': pickMethod({ GET: rateV2 }),
+      '/v2/exchangeRates': pickMethod({ POST: ratesV2 }),
+      '/v2/coinrank': pickMethod({ GET: sendCoinranksV2 }),
+      '/v2/coinrankAsset/([^/]+)': pickMethod({ GET: sendCoinrankAssetV2 }),
+      '/v2/coinrankList': pickMethod({ GET: sendCoinrankListV2 }),
+      '/v3/rates': pickMethod({ POST: ratesV3 })
+    })
+  )
 
   // Set up Express:
   const app = express()
